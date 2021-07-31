dd72de67df6bfb476a42cef2d180e84b1f256568
rumia.slt <rumia.slt@gmail.com>
2021-07-31 17:00:06 +0300

    add

---
+++ Assets/people.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class people : MonoBehaviour
+{
+    private int speed = 10;
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.back * speed * Time.deltaTime;
+        }
+    }
+}
