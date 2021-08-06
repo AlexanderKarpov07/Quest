72960aec4640d0fdc6ecf3c19fc6325375bf495f
rumia.slt <rumia.slt@gmail.com>
2021-08-06 16:46:26 +0300

    dergy

---
+++ Assets/door.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class door : MonoBehaviour
+{
+    [SerializeField]
+    private float openDoor;
+    [SerializeField]
+    private float closeDoor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.Slerp(Transform);
+    }
+}
